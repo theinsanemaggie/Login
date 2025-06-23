4a55f74fb5c85ce635e2edb25a834cca41b871d8
Theinsanemaggie <magalibelenamato@gmail.com>
2025-06-23 13:29:29 -0300

    Commit backup

--- obj/Debug/net8.0/LoginMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/LoginMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c54fe000bc6eddaf89dbac6d9f49a0c37e94da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df880eebda45f74b5fd9ef8caaa6e04322d7efe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
